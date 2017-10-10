936301b1960a81eb6686fa08586c40edd56a1d42
Scott Haselton <shaselton@gmail.com>
2017-10-10 13:00:49 -0400

    adding proto binaries to the kafka queue to be picked up later

--- lib/monolog_gateway.ex
+++ lib/monolog_gateway.ex
@@ -13,7 +13,31 @@ defmodule MonologGateway do
       :world
 
   """
-  def test do
-    Messages.Msg.new(version: :'V2', sub: 100)
+  def encode(message_value) do
+    test_message = Messages.Msg.new(version: :'V2', value: message_value)
+    encoded = Messages.Msg.encode(test_message)
+
+    message1 = %KafkaEx.Protocol.Produce.Message{value: encoded}
+    messages = [message1]
+
+    #snappy
+    produce_request = %KafkaEx.Protocol.Produce.Request{
+      topic: "test",
+      partition: 0,
+      required_acks: 1,
+      compression: :snappy,
+      messages: messages}
+    KafkaEx.produce(produce_request)
+  end
+
+  def decode(offset) do
+    KafkaEx.fetch("test", 0, offset: offset)
+    |> List.first
+    |> Map.get(:partitions) 
+    |> List.first 
+    |> Map.get(:message_set) 
+    |> List.first 
+    |> Map.get(:value)
+    |> Messages.Msg.decode
   end
 end
